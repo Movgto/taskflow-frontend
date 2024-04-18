2b85605340d64b1336414a68d8366a3429cda1f9
Gumaro Monroy <maromvz@gmail.com>
2024-04-18 13:58:30 -0600

    Add NotFound component for displaying a "not found" message

---
+++ src/views/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="block text-center text-xl font-bold text-indigo-700 py-10">
+      <p>The element that you wanted to see was not found in the database, sorry :&#40;</p>
+    </div>
+  )
+}
+
+export default NotFound
