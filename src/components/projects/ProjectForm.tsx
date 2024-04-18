4fd650ee5d9d96982132c00393d8ad8f060cade3
Gumaro Monroy <maromvz@gmail.com>
2024-04-18 15:58:29 -0600

    Add ProjectForm component for displaying a form to create a new project

---
+++ src/components/projects/ProjectForm.tsx
@@ -0,0 +1,72 @@
+import { FieldErrors, UseFormRegister } from "react-hook-form"
+import ErrorMessage from "../ErrorMessage"
+import { ProjectFormData } from "types"
+
+type ProjectFormProps = {
+  errors: FieldErrors<ProjectFormData>
+  register: UseFormRegister<ProjectFormData>
+}
+
+const ProjectForm = ({errors, register} : ProjectFormProps) => {
+  return (
+    <>
+      <div className="mb-5 space-y-3">
+          <label htmlFor="projectName" className="text-sm uppercase font-bold">
+              Nombre del Proyecto
+          </label>
+          <input
+              id="projectName"
+              className="w-full p-3  border border-gray-200"
+              type="text"
+              placeholder="Name of the Project"
+              {...register("projectName", {
+                  required: "Name of the Project is required",
+              })}
+          />
+
+          {errors.projectName && (
+              <ErrorMessage>{errors.projectName.message}</ErrorMessage>
+          )}
+      </div>
+
+      <div className="mb-5 space-y-3">
+          <label htmlFor="clientName" className="text-sm uppercase font-bold">
+              Nombre Cliente
+          </label>
+          <input
+              id="clientName"
+              className="w-full p-3  border border-gray-200"
+              type="text"
+              placeholder="Name of the Client"
+              {...register("clientName", {
+                  required: "Name of the Client is required",
+              })}
+          />
+
+          {errors.clientName && (
+              <ErrorMessage>{errors.clientName.message}</ErrorMessage>
+          )}
+      </div>
+
+      <div className="mb-5 space-y-3">
+          <label htmlFor="description" className="text-sm uppercase font-bold">
+              Descripción
+          </label>
+          <textarea
+              id="description"
+              className="w-full p-3  border border-gray-200"
+              placeholder="Description of the Project"
+              {...register("description", {
+                  required: "Description of the Project is required"
+              })}
+          />
+
+          {errors.description && (
+              <ErrorMessage>{errors.description.message}</ErrorMessage>
+          )}
+      </div>
+    </>
+  )
+}
+
+export default ProjectForm
