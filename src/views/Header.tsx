860d5acadca0823b8470021c36b9b43e67e7fec9
Gumaro Monroy <maromvz@gmail.com>
2024-04-20 15:01:26 -0600

    Update text color of the app name in Header component

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <header className='bg-slate-900 py-4'>
-      <div className='flex flex-col lg:flex-row items-center max-w-7xl mx-auto text-white font-black text-2xl lg:justify-between'>
+      <div className='flex flex-col lg:flex-row items-center
+                    max-w-7xl mx-auto text-zinc-50 font-black
+                    text-2xl lg:justify-between'>
         <Link to='/'>
           <h1 className="font-thin font-mono">Taskflow</h1>
         </Link>
