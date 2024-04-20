56fb4fb49fa24944c19970ec06200855d9af0646
Gumaro Monroy <maromvz@gmail.com>
2024-04-20 14:56:04 -0600

    Refactor TaskCard component to include delete functionality

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,18 +1,42 @@
+import { deleteTask } from "@/api/taskAPI"
 import { Task } from "@/types/index"
 import { Menu, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { Fragment } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type TaskCardProps = {
   task: Task
 }
 
 const TaskCard = ({ task } : TaskCardProps) => {
+
+  const navigate = useNavigate()
+
+  const params = useParams()
+
+  const projectId = params.id!
+
+  const queryClient = useQueryClient()
+
+  const { mutate: deleteTaskMutation } = useMutation({
+    mutationFn: deleteTask,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({queryKey: ['editProject', projectId]})
+      toast.success(data)
+    }
+  })
+
   return (
     <li
       className="bg-white border border-slate-300 flex justify-between p-2"
     >
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 text-pretty break-words w-[70%]">
         <button
           className="font-bold text-slate-600 text-left"
         >
@@ -23,9 +47,9 @@ const TaskCard = ({ task } : TaskCardProps) => {
         >{task.description}</p>
       </div>
 
-      <div className="flex shrink-0  gap-x-6">
+      <div className="flex">
           <Menu as="div" className="relative flex-none">
-              <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+              <Menu.Button className="text-gray-500 hover:text-gray-900">
                   <span className="sr-only">opciones</span>
                   <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
               </Menu.Button>
@@ -35,18 +59,30 @@ const TaskCard = ({ task } : TaskCardProps) => {
                   <Menu.Items
                       className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                       <Menu.Item>
-                          <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                          <button 
+                            type='button'
+                            className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                            onClick={() => navigate(location.pathname + `?seeTask=${task._id}`)}
+                          >
                               See
                           </button>
                       </Menu.Item>
                       <Menu.Item>
-                          <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                          <button
+                            type='button'
+                            className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                            onClick={() => navigate(location.pathname + `?editTask=true&taskId=${task._id}`)}
+                          >
                               Edit
                           </button>
                       </Menu.Item>
 
                       <Menu.Item>
-                          <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500'>
+                          <button
+                            type='button'
+                            className='block px-3 py-1 text-sm leading-6 text-red-500'
+                            onClick={() => deleteTaskMutation({projectId, taskId: task._id})}
+                          >
                               Delete
                           </button>
                       </Menu.Item>
