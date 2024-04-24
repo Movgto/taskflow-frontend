94d3657890d68c8b09e8eeaeafc76480e5c51613
Gumaro Monroy <maromvz@gmail.com>
2024-04-23 21:11:01 -0600

    Refactor AuthLayout component to include ToastContainer for displaying notifications

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,15 +1,17 @@
 import {Outlet} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 const AuthLayout = () => {
   return (
-    <div className='bg-slate-800 min-h-screen flex flex-col justify-center px-1'>
-      <div className='mx-auto flex flex-col justify-center gap-10'>
-        <h1 className='font-mono font-thin text-6xl text-zinc-50 text-center'>Taskflow</h1>
-        <div>
-          <Outlet />
+    <>
+      <div className='bg-slate-800 min-h-screen flex flex-col justify-center px-1'>
+        <div className='mx-auto flex flex-col justify-center gap-10 max-w-4xl'>
+          <h1 className='font-mono font-thin text-6xl text-zinc-50 text-center'>Taskflow</h1>          
+          <Outlet />          
         </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   )
 }
 
