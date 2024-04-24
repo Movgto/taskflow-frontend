d5b839fe7e27038b07e004a36e0c691df9400627
Gumaro Monroy <maromvz@gmail.com>
2024-04-23 21:09:59 -0600

    Update authAPI.ts to include signUp function for user registration

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,15 +1,29 @@
 import { isAxiosError } from "axios";
-import { LoginFormData } from "../types";
+import { LoginFormData, SignUpFormData } from "../types";
 import api from "@/lib/axios";
 
 
 export const loginAccount = async (formData : LoginFormData) => {
   try {
-    const {data} = await api.post('/auth/login', formData)
+    const url = '/auth/login'
+    const {data} = await api.post(url, formData)
 
     return data
   } catch (error) {
     console.log(error)
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export const signUp = async (formData: SignUpFormData) => {
+  try {
+    const url = '/auth/create-account'
+    const {data} = await api.post(url, formData)
+
+    return data
+  } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
     }
