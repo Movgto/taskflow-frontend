3e9636b2d373de467d60115ee459df3ce87078de
Gumaro Monroy <maromvz@gmail.com>
2024-04-27 14:50:23 -0600

    Add useAuth hook for handling user authentication

---
+++ src/hooks/auth/useAuth.ts
@@ -0,0 +1,16 @@
+import { getUser } from "@/api/authAPI"
+import { useQuery } from "@tanstack/react-query"
+
+
+const useAuth = () => {
+  const {data, isError, isLoading, error} = useQuery({
+    queryKey: ['user'],
+    queryFn: getUser,
+    retry: 1,
+    refetchOnWindowFocus: false
+  })
+
+  return {data, isError, isLoading, error}
+}
+
+export default useAuth
