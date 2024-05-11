8903eeced4961acd401c077e636e7056f6fab01e
Gumaro Monroy <maromvz@gmail.com>
2024-05-11 15:29:02 -0600

    refactor: Update ProjectDetailsView to display manager-specific options

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -4,12 +4,18 @@ import AddTaskModal from "@/components/tasks/AddTaskModal"
 import EditTaskData from "@/components/tasks/EditTaskData"
 import TaskDetailsModal from "@/components/tasks/TaskDetailsModal"
 import TaskList from "@/components/tasks/TaskList"
+import { isManager } from "@/helpers/auth"
+import useAuth from "@/hooks/auth/useAuth"
 import { useQuery } from "@tanstack/react-query"
-import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 const ProjectDetailsView = () => {
   const params = useParams()
-  const { data, isError, isLoading } = useQuery({
+
+  const {data: user, isLoading: authLoading} = useAuth()
+
+  const { data, isError, isLoading, error } = useQuery({
     queryKey: ['editProject', params.id],
     queryFn: () => getProjectById(params.id!)
   })
@@ -18,23 +24,37 @@ const ProjectDetailsView = () => {
 
   console.log(data, isError, isLoading)
 
-  if (isError) return <Navigate to="/404" />
-  if (isLoading) return <Loading message="Tasks" />
-  if (data) return (
+  if (isError) {
+    toast.error(error.message, {
+      toastId: 'projectError' + params.id
+    })
+    return <Navigate to="/404" />
+  }
+  if (isLoading || authLoading) return <Loading message="Tasks" />
+  if (data && user) return (
     <>
       <h2 className="text-4xl font-bold text-gray-800 my-2">{data.projectName}</h2>
-      <p className="text-gray-600 my-2">{data.description}</p>
-
-      <nav>
-        <button
-          type="button"
-          onClick={() => navigate('?newTask=true')}
-          className="bg-indigo-600 text-white font-bold p-3 text-xl
-                      rounded-md hover:bg-indigo-400 transition-colors"
-        >
-          Add a new Task
-        </button>
-      </nav>
+      <p className="text-gray-600 text-lg my-2">{data.description}</p>
+      {isManager(data.manager, user._id) && (
+        <>
+          <nav className="flex gap-4">
+            <button
+              type="button"
+              onClick={() => navigate('?newTask=true')}
+              className="bg-indigo-600 text-white font-bold p-3 text-xl
+                          rounded-md hover:bg-indigo-400 transition-colors"
+            >
+              Add a new Task
+            </button>
+            <Link
+              to='team'
+              className="bg-indigo-600 text-white font-bold p-3 text-xl
+                          rounded-md hover:bg-indigo-400 transition-colors"
+            >Members</Link>
+          </nav>
+        </>
+      )}
+      
       <TaskList tasks={data.tasks} />
       <AddTaskModal />
       <EditTaskData />
