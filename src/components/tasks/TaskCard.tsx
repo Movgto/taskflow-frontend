2b9ffdc6797b936120698fb08b2345d5762ada7c
Gumaro Monroy <maromvz@gmail.com>
2024-05-11 15:26:14 -0600

    feat: Add canEdit prop to TaskCard component

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -9,9 +9,10 @@ import { toast } from "react-toastify"
 
 type TaskCardProps = {
   task: Task
+  canEdit: boolean
 }
 
-const TaskCard = ({ task } : TaskCardProps) => {
+const TaskCard = ({ task, canEdit } : TaskCardProps) => {
 
   const navigate = useNavigate()
 
@@ -50,7 +51,7 @@ const TaskCard = ({ task } : TaskCardProps) => {
       <div className="flex">
           <Menu as="div" className="relative flex-none">
               <Menu.Button className="text-gray-500 hover:text-gray-900">
-                  <span className="sr-only">opciones</span>
+                  <span className="sr-only">options</span>
                   <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
               </Menu.Button>
               <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95"
@@ -67,25 +68,30 @@ const TaskCard = ({ task } : TaskCardProps) => {
                               See
                           </button>
                       </Menu.Item>
-                      <Menu.Item>
-                          <button
-                            type='button'
-                            className='block px-3 py-1 text-sm leading-6 text-gray-900'
-                            onClick={() => navigate(location.pathname + `?editTask=true&taskId=${task._id}`)}
-                          >
-                              Edit
-                          </button>
-                      </Menu.Item>
+                      {canEdit && (
+                        <>
+                          <Menu.Item>
+                              <button
+                                type='button'
+                                className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                onClick={() => navigate(location.pathname + `?editTask=true&taskId=${task._id}`)}
+                              >
+                                  Edit
+                              </button>
+                          </Menu.Item>
 
-                      <Menu.Item>
-                          <button
-                            type='button'
-                            className='block px-3 py-1 text-sm leading-6 text-red-500'
-                            onClick={() => deleteTaskMutation({projectId, taskId: task._id})}
-                          >
-                              Delete
-                          </button>
-                      </Menu.Item>
+                          <Menu.Item>
+                              <button
+                                type='button'
+                                className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                onClick={() => deleteTaskMutation({projectId, taskId: task._id})}
+                              >
+                                  Delete
+                              </button>
+                          </Menu.Item>
+                        </>
+                      )}
+                      
                   </Menu.Items>
               </Transition>
           </Menu>
