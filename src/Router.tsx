bfcc8a460f5ea096e3c6ba4f3b3f66abb30addd3
Gumaro Monroy <maromvz@gmail.com>
2024-05-11 15:25:15 -0600

    feat: Add TeamMembers view for project team management

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,6 +11,7 @@ import SignUp from './views/auth/SignUp'
 import AccountConfirmation from './views/auth/AccountConfirmation'
 import ForgotPassword from './views/auth/ForgotPassword'
 import ResetPassword from './views/auth/ResetPassword'
+import TeamMembers from './views/projects/TeamMembers'
 
 const Router = () => {
   return (
@@ -21,6 +22,7 @@ const Router = () => {
           <Route path='projects/create' element={<CreateProject />} />
           <Route path='projects/:id/edit' element={<EditProject />} />
           <Route path='projects/:id' element={<ProjectDetailsView />} />
+          <Route path='projects/:id/team' element={<TeamMembers />} />
           <Route path='404' element={<NotFound />} />
         </Route>
         <Route element={<AuthLayout />} path='auth'>
