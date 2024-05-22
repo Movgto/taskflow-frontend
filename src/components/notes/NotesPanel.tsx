7893f5210b36d0b85cf3d82fac30a752e2b6dd68
Gumaro Monroy <maromvz@gmail.com>
2024-05-21 18:22:57 -0600

    Add NotesPanel component for managing notes in a task

---
+++ src/components/notes/NotesPanel.tsx
@@ -0,0 +1,13 @@
+import AddNoteForm from "./AddNoteForm"
+import NotesList from "./NotesList"
+
+const NotesPanel = () => {
+  return (
+    <>      
+      <NotesList />
+      <AddNoteForm />
+    </>
+  )
+}
+
+export default NotesPanel
