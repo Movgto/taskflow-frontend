e230dc6a75f7796634fc8ee67c77724238c0ff57
Gumaro Monroy <maromvz@gmail.com>
2024-05-30 17:56:46 -0600

    Refactor ChangePasswordView component and improve password change UI

--- src/views/profile/ChangePasswordView.tsx
+++ src/views/profile/ChangePasswordView.tsx
@@ -1,8 +1,15 @@
+import ChangePasswordForm from "@/components/profile/ChangePasswordForm"
+
 const ChangePasswordView = () => {
   return (
-    <>
-      <p>Change Password View</p>
-    </>
+    <section className="max-w-4xl mx-auto mt-4 flex flex-col gap-2 items-center">
+      <h2 className="text-4xl font-bold text-gray-800">Change your password</h2>
+      <p className="text-gray-600">
+        Please enter your current password and the new password below.
+      </p>
+
+      <ChangePasswordForm />
+    </section>
   )
 }
 
